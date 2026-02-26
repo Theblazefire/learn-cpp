1e4b848e42ae49ba0df068a639d4b6ea1aab2fd0
Theblazefire <100469738+Theblazefire@users.noreply.github.com>
2026-02-26 18:31:08 +0100

    ex1 cpp stringhe

---
+++ ex generici/ex1 stringhe.cpp
@@ -0,0 +1,12 @@
+/*
+1. Addio char*
+Scrivi un programma che chiede all'utente il suo Nome e Cognome (separatamente) e il suo Anno di nascita.
+Il programma deve stampare: "Benvenuto [Nome] [Cognome], hai circa [X] anni."
+
+Vincolo: Usa solo std::string, std::cin, std::cout. Niente char[] o printf.
+*/
+#include <iostream>
+int main(){
+    std::cout<<"ciao il tuo nome /n"std::endl;
+
+}
